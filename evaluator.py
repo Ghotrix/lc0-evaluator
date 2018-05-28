a1139fdd19d85d363b0f46d2690a05b1a0bee0c4
Valeriy Huz <ghotrix@gmail.com>
2018-05-28 14:46:21 +0300

    preliminary work on evaluator

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,21 @@
 import glob
 import re
+from random import sample
+from chess import pgn
+import subprocess
 
+tournament_pgn = 't.pgn'
 
 weight_files = glob.glob('*.txt.gz')
 
 weights = sorted([int(re.search('(.*)_(\d*)\.txt\.gz', file).group(2)) for file in weight_files])
 
-print(weights)
+try:
+    with open(tournament_pgn) as pgn_file:
+        pass
+except FileNotFoundError:
+    pass
+
+rand_nets = sample(weights, 2)
+
+#subprocess.run(['cutechess-cli', '-pgnout', tournament_pgn, '-engine', ])
