bb659f53c3e6cf7af3c476745ea12a53a04f978b
Valerii Huz <ext-valerii.huz@here.com>
2018-06-01 11:50:37 +0300

    added equalish opponents selection to evaluator

--- evaluator.py
+++ evaluator.py
@@ -1,36 +1,43 @@
 import glob
 import re
 import os
-from random import sample, choice
-#from chess import pgn
+from random import choice
+from chess import pgn
 import subprocess
-import bisect
-from collections import OrderedDict
+
+def get_game_point_ratio(wnp):
+    games_num = wnp[1]
+    points = wnp[2]
+    if games_num == 0:
+        return 1.
+    else:
+        return points/float(games_num)
 
 
-def get_near_points_opponent(player, players):
-    points_value = players[player][1]
-    ordered_players = OrderedDict(sorted(players.items(), key=lambda t: t[1][1]))
-    del ordered_players[player]
-    weights = []
-    points = []
-    for weight, stats in ordered_players.items():
-        weights.append(weight)
-        points.append(stats[1])
-    near_point = bisect.bisect_left(points, points_value)
-    try:
-        return weights[near_point]
-    except IndexError:
-        return choice(weights)
+def sample_two_equal_opponents(weight_points):
+    random_weight = choice(weight_points)
+
+    ordered_by_points = sorted(weight_points, key=get_game_point_ratio)
+    first_idx = ordered_by_points.index(random_weight)
+    second_idx = first_idx + 1
+    if second_idx >= len(ordered_by_points):
+        return [ordered_by_points[first_idx], ordered_by_points[second_idx - 2]]
+    else:
+        return [ordered_by_points[first_idx], ordered_by_points[second_idx]]
 
 
 def get_least_played_equal_opponents(players):
-    least_played = [[k, v[1]] for k, v in players.items() if v[0] == players[min(players, key=players.get)]]
-    equal_opponents = []
+    least_played = [[k, v[0], v[1]] for k, v in players.items()
+                    if v[0] == min(players.items(), key=lambda x: x[1][0])[1][0]]
+    rest_played = [[k, v[0], v[1]] for k, v in players.items()] - least_played
+
     if len(least_played) == 1:
-        least_played.append(get_near_points_opponent(least_played[0], players))
-    print('least played nets: {}'.format(least_played))
-    return least_played
+        least_played += [choice(rest_played)]
+        assert len(least_played == 2)
+        return [w for w, n, p in least_played]
+    equal_opponents = sample_two_equal_opponents(least_played)
+    print('least played equal-ish nets: {}'.format(equal_opponents))
+    return equal_opponents
 
 
 def get_points_from_result(result):
@@ -45,7 +52,7 @@ def get_points_from_result(result):
     return points
 
 
-'''tournament_pgn = 't.pgn'
+tournament_pgn = 't.pgn'
 
 weight_files = glob.glob('*.txt.gz')
 
@@ -75,19 +82,12 @@ cur_dir = os.getcwd()
 
 while True:
     least_played = get_least_played_equal_opponents(players)
-    rand_nets = sample(least_played, 2)
-    players[rand_nets[0]] += 1
-    players[rand_nets[1]] += 1
-    print('game will be played between {} and {}'.format(rand_nets[0], rand_nets[1]))
+    players[least_played[0]][0] += 1
+    players[least_played[1]][0] += 1
+    print('game will be played between {} and {}'.format(least_played[0], least_played[1]))
     result = subprocess.run('cutechess-cli -pgnout {3} -engine name={0} cmd=./lc0.sh \
                              arg="--weights={2}/weights_{0}.txt.gz" -engine name={1} cmd=./lc0.sh \
                              arg="--weights={2}/weights_{1}.txt.gz" \
-                             -each proto=uci nodes=800 tc=inf'.format(rand_nets[0], rand_nets[1],
+                             -each proto=uci nodes=800 tc=inf'.format(least_played[0], least_played[1],
                                                                       cur_dir, tournament_pgn),
                             stdout=subprocess.PIPE, shell=True)
-'''
-
-if __name__ == '__main__':
-    players = {1: [2, 1], 2: [3, 0], 3: [2, 0], 4: [2, 2]}
-    x = get_near_points_opponent(2, players)
-    print(x)
